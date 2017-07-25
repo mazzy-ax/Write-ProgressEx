cd369afd6888f79a01d7f878d988a610e23e551a
mazzy <mazzy@mazzy.ru>
2017-07-25 10:14:13 +0300

    v0.6

--- write-ProgressEx.psm1
+++ write-ProgressEx.psm1
@@ -113,7 +113,7 @@ function Write-ProgressEx {
                 -and ($pInfo.Total -gt 0) `
                 -and ($pInfo.Current -ne -1) ) {
             # next
-            $pInfo.Current = [Math]::Max(0, [Math]::Min($pInfo.Current + 1, $pInfo.Total))
+            $pInfo.Current = [Math]::Min([Math]::Max(0, $pInfo.Current + 1), $pInfo.Total)
 
             # calc
             if ( -not $PercentComplete ) {
