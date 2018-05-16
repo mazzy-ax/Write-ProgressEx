c908609b891c7e94ca771484624c24f6443e80c2
Sergey Mazurkin <mazzy@mazzy.ru>
2018-05-16 22:47:21 +0300

    appveyor

--- Tests/psScriptAnalyzer.Tests.ps1
+++ Tests/psScriptAnalyzer.Tests.ps1
@@ -4,13 +4,9 @@ $moduleName = Split-Path $moduleRoot -Leaf
 
 Describe "PSScriptAnalyzer Rules for $moduleName" -Tag Meta, BestPractice, BP {
     $analysis = Invoke-ScriptAnalyzer -Path $projectRoot -Recurse
-    $scriptAnalyzerRules = Get-ScriptAnalyzerRule
-    forEach ($rule in $scriptAnalyzerRules) {
-        It "Should pass $rule" {
-            If (($analysis) -and ($analysis.RuleName -contains $rule)) {
-                $analysis | Where-Object RuleName -EQ $rule -OutVariable failures | Out-Default
-                $failures.Count | Should Be 0
-            }
-        }
+
+    It "Should be Ok" {
+        $analysis | Should -BeNullOrEmpty
     }
+
 }
